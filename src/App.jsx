370e2e82ffc21df7dde5c77c88fc5c5e268a0e9a
Jun <peng940825@gmail.com>
2023-05-06 23:46:58 +0800

    add current device

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import currentDevice from 'current-device';
 
-function App() {
-  const [count, setCount] = useState(0)
+import IconCheck from '@/icons/IconCheck';
+import IconXmark from '@/icons/IconXmark';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const valMap = {
+    string: (key) => currentDevice[key],
+    function: (key) => currentDevice[key](),
+  };
+
+  const reorgData = () => {
+    const arr = [];
+    for (const key in currentDevice) {
+      if (
+        key !== 'cordova' &&
+        key !== 'noConflict' &&
+        key !== 'onChangeOrientation'
+      ) {
+        let val = valMap[typeof currentDevice[key]](key);
+        if (typeof val === 'boolean' && val) {
+          val = <IconCheck width="32px" height="32px" fill="#31b27c" />;
+        }
+        if (typeof val === 'boolean' && !val) {
+          val = <IconXmark width="32px" height="32px" fill="#aa0d31" />;
+        }
+        arr.push({ key, val });
+      }
+    }
+    setData(arr);
+  };
+
+  useEffect(() => {
+    reorgData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      {data.reverse().map((item) => (
+        <p key={item.key}>
+          {item.key}：{item.val}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { URL, fileURLToPath } from 'node:url';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/current-device/',
+  build: {
+    outDir: 'docs',
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
